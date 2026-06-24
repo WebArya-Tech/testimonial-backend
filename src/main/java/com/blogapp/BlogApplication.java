62a9171580f32ca8981bd004d5091865f44ec134
saturnrings1979-titan <saturnrings1979@gmail.com>
2026-06-25 00:27:00 +0530

    feat: integrate Mongock for database migrations and implement conditional password management for OTP users

--- src/main/java/com/blogapp/BlogApplication.java
+++ src/main/java/com/blogapp/BlogApplication.java
@@ -2,7 +2,9 @@ package com.blogapp;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import io.mongock.runner.springboot.EnableMongock;
 
+@EnableMongock
 @SpringBootApplication
 public class BlogApplication {
 
